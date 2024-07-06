10ead08cbf23a11786e5304c88d7bec29c11494b
ehhugo <borgiani.hugo@gmail.com>
2024-07-05 23:33:25 -0300

    updated deploy file

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -20,22 +20,16 @@ echo "Deploying catalog..."
 # Desplegar catalog
 cd dist/apps/catalog
 
-# Sync bundles with strong cache
-aws s3 sync ./static/css s3://$S3_ORIGIN_BUCKET/catalog/static/css --metadata-directive 'REPLACE' --cache-control max-age=31536000,public,immutable --delete
-aws s3 sync ./static/js s3://$S3_ORIGIN_BUCKET/catalog/static/js --metadata-directive 'REPLACE' --cache-control max-age=31536000,public,immutable --delete
-
-# Sync HTML and other files with no cache
-aws s3 sync ./ s3://$S3_ORIGIN_BUCKET/catalog --exclude "static/*" --metadata-directive 'REPLACE' --cache-control no-cache,no-store,must-revalidate --delete
+# Sync files with appropriate cache settings
+aws s3 sync ./ s3://$S3_ORIGIN_BUCKET/catalog --metadata-directive 'REPLACE' --cache-control max-age=31536000,public,immutable --exclude "index.html" --exclude "assets/*"
+aws s3 sync ./ s3://$S3_ORIGIN_BUCKET/catalog --metadata-directive 'REPLACE' --cache-control no-cache,no-store,must-revalidate --include "index.html" --include "assets/*" --exclude "*"
 
 echo "Deploying checkout..."
 # Desplegar checkout
 cd ../../checkout
 
-# Sync bundles with strong cache
-aws s3 sync ./static/css s3://$S3_ORIGIN_BUCKET/checkout/static/css --metadata-directive 'REPLACE' --cache-control max-age=31536000,public,immutable --delete
-aws s3 sync ./static/js s3://$S3_ORIGIN_BUCKET/checkout/static/js --metadata-directive 'REPLACE' --cache-control max-age=31536000,public,immutable --delete
-
-# Sync HTML and other files with no cache
-aws s3 sync ./ s3://$S3_ORIGIN_BUCKET/checkout --exclude "static/*" --metadata-directive 'REPLACE' --cache-control no-cache,no-store,must-revalidate --delete
+# Sync files with appropriate cache settings
+aws s3 sync ./ s3://$S3_ORIGIN_BUCKET/checkout --metadata-directive 'REPLACE' --cache-control max-age=31536000,public,immutable --exclude "index.html" --exclude "assets/*"
+aws s3 sync ./ s3://$S3_ORIGIN_BUCKET/checkout --metadata-directive 'REPLACE' --cache-control no-cache,no-store,must-revalidate --include "index.html" --include "assets/*" --exclude "*"
 
 echo "Despliegue completado exitosamente."
