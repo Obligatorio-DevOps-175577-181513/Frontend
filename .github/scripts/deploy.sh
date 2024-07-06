e538f374c8619000cc31556642caaa1b3438f362
ehhugo <borgiani.hugo@gmail.com>
2024-07-05 22:47:02 -0300

    updated deploy file

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -25,3 +25,12 @@ aws s3 sync ./ s3://$S3_ORIGIN_BUCKET/catalog --exclude "static/*" --metadata-di
 
 # Desplegar checkout
 cd ../../checkout
+
+# Sync bundles with strong cache
+aws s3 sync ./static/css s3://$S3_ORIGIN_BUCKET/checkout/static/css --metadata-directive 'REPLACE' --cache-control max-age=31536000,public,immutable --delete
+aws s3 sync ./static/js s3://$S3_ORIGIN_BUCKET/checkout/static/js --metadata-directive 'REPLACE' --cache-control max-age=31536000,public,immutable --delete
+
+# Sync HTML and other files with no cache
+aws s3 sync ./ s3://$S3_ORIGIN_BUCKET/checkout --exclude "static/*" --metadata-directive 'REPLACE' --cache-control no-cache,no-store,must-revalidate --delete
+
+echo "Despliegue completado exitosamente."
